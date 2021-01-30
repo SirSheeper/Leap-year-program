8a00ed2c1f6d5c4498ad76befea740cd11af018c
SirSheeper <Beanjacob25@gmail.com>
2021-01-30 14:54:24 -0800

    Add files via upload

---
+++ jacob_bean_hw1_error_handling.py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Jacob
+#
+# Created:     15/01/2021
+# Copyright:   (c) Jacob 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+def isLeap(year):
+    if(year%100 == 0):
+        if((year >=400) and (year%400 == 0)):
+            return True
+        return False
+    elif(year%4 == 0):
+        return True
+    else:
+        return False
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+year = input("enter year to check for leep year:")
+while(year.isdigit() == False):
+    year = input("Not valid input, please use numbers")
+
+year = int(year)
+
+while (year <=0):
+    year = int(input("Invalid Year, please enter a new year above 0:"))
+
+
+print("is the year "+ str(year) +" a leap year?")
+
+if(isLeap(year) == True):
+    print ("yes")
+else:
+    print("no")
+
+input("Press Enter to exit")
